491076eced5cca29aee65e534173e330dfefd7ff
sdbabi <sd671990@gmail.com>
2019-01-16 00:08:38 +0530

    final update by sd

--- app/src/main/java/com/swagger/app/PartnerListActivity.java
+++ app/src/main/java/com/swagger/app/PartnerListActivity.java
@@ -4,18 +4,19 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.RequestParams;
 import com.loopj.android.http.TextHttpResponseHandler;
-import com.swagger.app.adapter.ServiceAdapter;
-import com.swagger.app.model.ServiceCategoryModel;
+import com.swagger.app.adapter.PartnerAdapter;
+import com.swagger.app.model.PartnerModel;
 import com.swagger.app.utils.SharedPreferenceClass;
 import com.swagger.app.utils.StaticVariables;
 
@@ -23,14 +24,13 @@ import org.apache.http.Header;
 
 import java.util.List;
 
-public class UserActivity extends AppCompatActivity {
-
+public class PartnerListActivity extends AppCompatActivity {
     Toolbar toolbar;
-    RecyclerView serviceList;
-    ProgressBar pBar;
-    SharedPreferenceClass sharedPreferenceClass;
+    TextView topTxt;
+    RecyclerView partnerList;
     Activity mActivity;
-
+    SharedPreferenceClass sharedPreferenceClass;
+    ProgressBar pBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,30 +39,38 @@ public class UserActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         initWidgets();
-        if (Common.checkNetworkConnection(this)) {
-            populateServiceList();
-        } else {
-            Toast.makeText(this, "Please check internet connection & try again", Toast.LENGTH_SHORT).show();
+        String serviceId = getIntent().getStringExtra("serviceId");
+        if (serviceId != null) {
+            if (Common.checkNetworkConnection(this)) {
+                populatePartnerList(serviceId);
+            } else {
+                Toast.makeText(this, "Please check your internet connection", Toast.LENGTH_SHORT).show();
+            }
         }
 
     }
 
     private void initWidgets() {
-        mActivity = UserActivity.this;
+        mActivity = PartnerListActivity.this;
         sharedPreferenceClass = new SharedPreferenceClass(mActivity);
-        serviceList = findViewById(R.id.serviceList);
+        topTxt = findViewById(R.id.topText);
+        partnerList = findViewById(R.id.serviceList);
         pBar = findViewById(R.id.pBar);
+        topTxt.setText("Partner Details");
     }
 
-    private void populateServiceList() {
+
+    private void populatePartnerList(String serviceId) {
         AsyncHttpClient client = new AsyncHttpClient();
         client.setTimeout(30000);
+        RequestParams params = new RequestParams();
 
         // add headers
         client.addHeader("Authorization", "bearer " + sharedPreferenceClass.getValue_string(StaticVariables.ACCESS_TOKEN));
         client.addHeader("Content-Type", "application/json");
 
-        client.get(Common.productCategoryUrl, new TextHttpResponseHandler() {
+        //params.put("productId", serviceId);
+        client.get(Common.partnerListUrl + "/" + serviceId, new TextHttpResponseHandler() {
             @Override
             public void onStart() {
                 super.onStart();
@@ -77,22 +85,22 @@ public class UserActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(int i, Header[] headers, String s, Throwable throwable) {
-                Toast.makeText(UserActivity.this, "Sorry, Something went wrong", Toast.LENGTH_SHORT).show();
+                Toast.makeText(PartnerListActivity.this, "Sorry, no partners available", Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onSuccess(int i, Header[] headers, String response) {
                 try {
-                    List<ServiceCategoryModel> sList = ServiceCategoryModel.populateFromDownload(response);
+                    List<PartnerModel> sList = PartnerModel.populateFromDownload(response);
                     if (sList.size() == 0) {
-                        Toast.makeText(UserActivity.this, "Sorry, unable to fetch list", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(PartnerListActivity.this, "Sorry, unable to fetch list", Toast.LENGTH_SHORT).show();
 
                     } else {
-                        final GridLayoutManager glm = new GridLayoutManager(UserActivity.this, 2, LinearLayoutManager.HORIZONTAL, false);
-                        glm.setOrientation(LinearLayoutManager.VERTICAL);
-                        ServiceAdapter pAdapter = new ServiceAdapter(UserActivity.this, sList);
-                        serviceList.setLayoutManager(glm);
-                        serviceList.setAdapter(pAdapter);
+                        final LinearLayoutManager llm = new LinearLayoutManager(PartnerListActivity.this);
+                        llm.setOrientation(LinearLayoutManager.VERTICAL);
+                        PartnerAdapter pAdapter = new PartnerAdapter(PartnerListActivity.this, sList);
+                        partnerList.setLayoutManager(llm);
+                        partnerList.setAdapter(pAdapter);
                         pBar.setVisibility(View.GONE);
                     }
                 } catch (Exception e) {
@@ -102,10 +110,4 @@ public class UserActivity extends AppCompatActivity {
         });
     }
 
-
-
-    @Override
-    public void onBackPressed() {
-        finish();
-    }
 }
--- app/src/main/java/com/swagger/app/adapter/ServiceAdapter.java
+++ app/src/main/java/com/swagger/app/adapter/ServiceAdapter.java
@@ -1,6 +1,7 @@
 package com.swagger.app.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
+import com.swagger.app.PartnerListActivity;
 import com.swagger.app.R;
 import com.swagger.app.model.ServiceCategoryModel;
 
@@ -43,7 +45,9 @@ public class ServiceAdapter extends RecyclerView.Adapter<ServiceAdapter.Recycler
         holder.containerlayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(ctx, "Work in progress", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(ctx, PartnerListActivity.class);
+                i.putExtra("serviceId", sModel.getId());
+                ctx.startActivity(i);
             }
         });
     }
